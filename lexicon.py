a8f464ec3206ed5e3b460121ccb4c00fd04a02fd
Hugo Zhang <hugozhang2023@u.northwestern.edu>
2023-03-12 12:04:15 -0500

    need to add more substitutions but works alright for ingredients, needs more work for steps

--- lexicon.py
+++ lexicon.py
@@ -6,6 +6,7 @@ meat_substitutions = {
     "beef stock": { "substitution": "vegetable stock", "ratio": 1.0, "unit": None, "sentence": None, "comment": None, "additions": { "prep_time": 0, "ingredients": [], "steps": []} },
     "beef broth": { "substitution": "vegetable broth", "ratio": 1.0, "unit": None, "sentence": None, "comment": None, "additions": { "prep_time": 0, "ingredients": [], "steps": []} },
     "ground beef": { "substitution": "brown lentils", "ratio": 1.0, "unit": "cup", "sentence": " cup(s) of dried, uncooked brown lentils", "comment": "dried, uncooked", "additions": { "prep_time": 15, "ingredients": ["3 cups water"], "steps": ["Place the lentils and water in a large saucepan.", "Bring to a rapid simmer, then reduce the heat and simmer for about 15 minutes until the lentils are tender but still hold their shape. Drain any excess liquid and set aside for later use."] }},
+    "beef patties": { "substitution": "bean patties", "ratio": 1.0, "unit": None, "sentence": None, "comment": None, "additions": { "prep_time": 0, "ingredients": [], "steps": []} },
     "beef": { "substitution": "tempeh", "ratio": 1.0, "unit": None, "sentence": None, "comment": None, "additions": { "prep_time": 0, "ingredients": [], "steps": []} },
     "chicken stock": { "substitution": "vegetable stock", "ratio": 1.0, "unit": None, "sentence": None, "comment": None, "additions": { "prep_time": 0, "ingredients": [], "steps": []} },
     "chicken broth": { "substitution": "vegetable broth", "ratio": 1.0, "unit": None, "sentence": None, "comment": None, "additions": { "prep_time": 0, "ingredients": [], "steps": []} },
@@ -22,9 +23,6 @@ meat_substitutions = {
     "veal": { "substitution": "beets", "ratio": 1.0, "unit": None, "sentence": None, "comment": None, "additions": { "prep_time": 0, "ingredients": [], "steps": []} },
     "bone broth": { "substitution": "vegetable broth", "ratio": 1.0, "unit": None, "sentence": None, "comment": None, "additions": { "prep_time": 0, "ingredients": [], "steps": []} },
     "bones": { "substitution": "vegetable scraps", "ratio": 1.0, "unit": None, "sentence": None, "comment": None, "additions": { "prep_time": 0, "ingredients": [], "steps": []} },
-}
-
-seafood_substitutions = {
     "lobster": { "substitution": "vegan lobster", "ratio": 1.0, "unit": None, "sentence": None, "comment": None, "additions": { "prep_time": 0, "ingredients": [], "steps": []} },
     "crab": { "substitution": "vegan crab", "ratio": 1.0, "unit": None, "sentence": None, "comment": None, "additions": { "prep_time": 0, "ingredients": [], "steps": []} },
     "shrimp": { "substitution": "vegan shrimp", "ratio": 1.0, "unit": None, "sentence": None, "comment": None, "additions": { "prep_time": 0, "ingredients": [], "steps": []} },
@@ -32,7 +30,7 @@ seafood_substitutions = {
 }
 
 vegetable_substitutions = {
-
+    "": { "substitution": "", "ratio": 1.0, "unit": None, "sentence": None, "comment": None, "additions": { "prep_time": 0, "ingredients": [], "steps": []} },
 }
 
 healthy_substitutions = {
@@ -41,7 +39,44 @@ healthy_substitutions = {
 }
 
 unhealthy_substitutions = {
-    "butter": { "substitution": "", "ratio": 1.0, "unit": None, "sentence": None, "comment": None, "additions": { "prep_time": 0, "ingredients": [], "steps": []} },
+    "butter": { "substitution": "coconut oil", "ratio": 1.0, "unit": None, "sentence": None, "comment": None, "additions": { "prep_time": 0, "ingredients": [], "steps": []} },
+    "margarine": { "substitution": "coconut oil", "ratio": 1.0, "unit": None, "sentence": None, "comment": None, "additions": { "prep_time": 0, "ingredients": [], "steps": []} },
+    "shortening": { "substitution": "coconut oil", "ratio": 1.0, "unit": None, "sentence": None, "comment": None, "additions": { "prep_time": 0, "ingredients": [], "steps": []} },
+    "lard": { "substitution": "coconut oil", "ratio": 1.0, "unit": None, "sentence": None, "comment": None, "additions": { "prep_time": 0, "ingredients": [], "steps": []} },
+    "palm oil": { "substitution": "olive oil", "ratio": 1.0, "unit": None, "sentence": None, "comment": "extra virgin", "additions": { "prep_time": 0, "ingredients": [], "steps": []} },
+    "corn oil": { "substitution": "olive oil", "ratio": 1.0, "unit": None, "sentence": None, "comment": "extra virgin", "additions": { "prep_time": 0, "ingredients": [], "steps": []} },
+    "canola oil": { "substitution": "olive oil", "ratio": 1.0, "unit": None, "sentence": None, "comment": "extra virgin", "additions": { "prep_time": 0, "ingredients": [], "steps": []} },
+    "peanut oil": { "substitution": "olive oil", "ratio": 1.0, "unit": None, "sentence": None, "comment": "extra virgin", "additions": { "prep_time": 0, "ingredients": [], "steps": []} },
+    "vegetable oil": { "substitution": "olive oil", "ratio": 1.0, "unit": None, "sentence": None, "comment": "extra virgin", "additions": { "prep_time": 0, "ingredients": [], "steps": []} },
     "white rice": { "substitution": "brown rice", "ratio": 1.0, "unit": None, "sentence": None, "comment": None, "additions": { "prep_time": 0, "ingredients": [], "steps": []} },
-    "": { "substitution": "", "ratio": 1.0, "unit": None, "sentence": None, "comment": None, "additions": { "prep_time": 0, "ingredients": [], "steps": []} },
+    "all purpose flour": { "substitution": "whole wheat flour", "ratio": 1.0, "unit": None, "sentence": None, "comment": None, "additions": { "prep_time": 0, "ingredients": [], "steps": []} },
+    "all-purpose flour": { "substitution": "whole wheat flour", "ratio": 1.0, "unit": None, "sentence": None, "comment": None, "additions": { "prep_time": 0, "ingredients": [], "steps": []} },
+    "white flour": { "substitution": "whole wheat flour", "ratio": 1.0, "unit": None, "sentence": None, "comment": None, "additions": { "prep_time": 0, "ingredients": [], "steps": []} },
+    "white bread": { "substitution": "whole wheat bread", "ratio": 1.0, "unit": None, "sentence": None, "comment": None, "additions": { "prep_time": 0, "ingredients": [], "steps": []} },
+    "white sugar": { "substitution": "honey", "ratio": 0.5, "unit": None, "sentence": None, "comment": None, "additions": { "prep_time": 0, "ingredients": [], "steps": []} },
+    "brown sugar": { "substitution": "coconut sugar", "ratio": 1.0, "unit": None, "sentence": None, "comment": None, "additions": { "prep_time": 0, "ingredients": [], "steps": []} },
+    "high-fructose corn syrup": { "substitution": "honey", "ratio": 1.0, "unit": None, "sentence": None, "comment": None, "additions": { "prep_time": 0, "ingredients": [], "steps": []} },
+    "corn syrup": { "substitution": "honey", "ratio": 1.0, "unit": None, "sentence": None, "comment": None, "additions": { "prep_time": 0, "ingredients": [], "steps": []} },
+    "maple syrup": { "substitution": "honey", "ratio": 1.0, "unit": None, "sentence": None, "comment": None, "additions": { "prep_time": 0, "ingredients": [], "steps": []} },
+    "simple syrup": { "substitution": "honey", "ratio": 1.0, "unit": None, "sentence": None, "comment": None, "additions": { "prep_time": 0, "ingredients": [], "steps": []} },
+    "breadcrumbs": { "substitution": "crushed nuts", "ratio": 1.0, "unit": None, "sentence": None, "comment": None, "additions": { "prep_time": 5, "ingredients": [], "steps": []} },
+    "panko breadcrumbs": { "substitution": "crushed nuts", "ratio": 1.0, "unit": None, "sentence": None, "comment": None, "additions": { "prep_time": 5, "ingredients": [], "steps": []} },
+    "bread crumbs": { "substitution": "crushed nuts", "ratio": 1.0, "unit": None, "sentence": None, "comment": None, "additions": { "prep_time": 5, "ingredients": [], "steps": []} },
+    "panko bread crumbs": { "substitution": "crushed nuts", "ratio": 1.0, "unit": None, "sentence": None, "comment": None, "additions": { "prep_time": 5, "ingredients": [], "steps": []} },
+    "sour cream": { "substitution": "greek yogurt", "ratio": 1.0, "unit": None, "sentence": None, "comment": "plain", "additions": { "prep_time": 0, "ingredients": [], "steps": []} },
+    "mayonnaise": { "substitution": "greek yofurt", "ratio": 1.0, "unit": None, "sentence": None, "comment": "plain", "additions": { "prep_time": 0, "ingredients": [], "steps": []} },
+    "cream cheese": { "substitution": "cottage cheese", "ratio": 1.0, "unit": None, "sentence": None, "comment": None, "additions": { "prep_time": 0, "ingredients": [], "steps": []} },
+    "heavy cream": { "substitution": "coconut cream", "ratio": 1.0, "unit": None, "sentence": None, "comment": None, "additions": { "prep_time": 0, "ingredients": [], "steps": []} },
+    "ground beef": { "substitution": "ground turkey", "ratio": 1.0, "unit": None, "sentence": None, "comment": None, "additions": { "prep_time": 0, "ingredients": [], "steps": []} },
+    "beef stock": { "substitution": "vegetable stock", "ratio": 1.0, "unit": None, "sentence": None, "comment": None, "additions": { "prep_time": 0, "ingredients": [], "steps": []} },
+    "beef broth": { "substitution": "vegetable broth", "ratio": 1.0, "unit": None, "sentence": None, "comment": None, "additions": { "prep_time": 0, "ingredients": [], "steps": []} },
+    "beef chuck": { "substitution": "turkey thigh", "ratio": 1.0, "unit": None, "sentence": None, "comment": None, "additions": { "prep_time": 0, "ingredients": [], "steps": []} },
+    "beef chuck roast": { "substitution": "turkey thigh", "ratio": 1.0, "unit": None, "sentence": None, "comment": None, "additions": { "prep_time": 0, "ingredients": [], "steps": []} },
+    "beef": { "substitution": "turkey", "ratio": 1.0, "unit": None, "sentence": None, "comment": None, "additions": { "prep_time": 0, "ingredients": [], "steps": []} },
+    "ground pork": { "substitution": "ground turkey", "ratio": 1.0, "unit": None, "sentence": None, "comment": None, "additions": { "prep_time": 0, "ingredients": [], "steps": []} },
+    "pork sausage": { "substitution": "turkey sausage", "ratio": 1.0, "unit": None, "sentence": None, "comment": None, "additions": { "prep_time": 0, "ingredients": [], "steps": []} },
+    "pork": { "substitution": "turkey", "ratio": 1.0, "unit": None, "sentence": None, "comment": None, "additions": { "prep_time": 0, "ingredients": [], "steps": []} },
+    "bacon": { "substitution": "turkey bacon", "ratio": 1.0, "unit": None, "sentence": None, "comment": None, "additions": { "prep_time": 0, "ingredients": [], "steps": []} },
 }
+
+# "": { "substitution": "", "ratio": 1.0, "unit": None, "sentence": None, "comment": None, "additions": { "prep_time": 0, "ingredients": [], "steps": []} },
--- transform.py
+++ transform.py
@@ -1,70 +1,9 @@
 from recipe_scrapers import scrape_me
 from ingredient_parser import parse_multiple_ingredients, parse_ingredient
+from lexicon import meat_substitutions, unhealthy_substitutions
 import pprint
 
-scraper = scrape_me("https://www.allrecipes.com/recipe/285077/easy-one-pot-ground-turkey-pasta/")
-
-# Original value recipe values.
-original_recipe_prep_time = scraper.total_time()
-# unprocessed_ingredients = scraper.ingredients()
-# original_recipe_ingredients = [x.lower() for x in unprocessed_ingredients]
-original_recipe_ingredients = scraper.ingredients()
-original_recipe_parsed_ingredients = parse_multiple_ingredients(original_recipe_ingredients)
-original_recipe_instructions = scraper.instructions_list()
-
-
-# Updated value recipe values.
-updated_recipe_prep_time = original_recipe_prep_time
-updated_recipe_ingredients = original_recipe_ingredients.copy()
-updated_recipe_parsed_ingredients = sorted(original_recipe_parsed_ingredients.copy(), key=lambda d: d['name'])
-updated_recipe_instructions = original_recipe_instructions.copy()
-
-print(updated_recipe_parsed_ingredients)
-meat_products = ["beef", "chicken", "pork", "lamb", "goat", "turkey", "veal", "duck", "goose", "rabbit", "sausage", "hot dog", "bratwurst", "cow", "pig", "liver", "tripe", "offal", "gelatin"]
-
-meat_substitutions = {
-    "beef stock": { "substitution": "vegetable stock", "ratio": 1.0, "unit": None, "sentence": None, "comment": None, "additions": { "prep_time": 0, "ingredients": [], "steps": []} },
-    "beef broth": { "substitution": "vegetable broth", "ratio": 1.0, "unit": None, "sentence": None, "comment": None, "additions": { "prep_time": 0, "ingredients": [], "steps": []} },
-    "ground beef": { "substitution": "brown lentils", "ratio": 1.0, "unit": "cup", "sentence": " cup(s) of dried, uncooked brown lentils", "comment": "dried, uncooked", "additions": { "prep_time": 15, "ingredients": ["3 cups water"], "steps": ["Place the lentils and water in a large saucepan.", "Bring to a rapid simmer, then reduce the heat and simmer for about 15 minutes until the lentils are tender but still hold their shape. Drain any excess liquid and set aside for later use."] }},
-    "beef": { "substitution": "tempeh", "ratio": 1.0, "unit": None, "sentence": None, "comment": None, "additions": { "prep_time": 0, "ingredients": [], "steps": []} },
-    "chicken stock": { "substitution": "vegetable stock", "ratio": 1.0, "unit": None, "sentence": None, "comment": None, "additions": { "prep_time": 0, "ingredients": [], "steps": []} },
-    "chicken broth": { "substitution": "vegetable broth", "ratio": 1.0, "unit": None, "sentence": None, "comment": None, "additions": { "prep_time": 0, "ingredients": [], "steps": []} },
-    "chicken breast": { "substitution": "seitan slice", "ratio": 1.0, "unit": None, "sentence": None, "comment": None, "additions": { "prep_time": 0, "ingredients": [], "steps": []} },
-    "chicken thigh": { "substitution": "seitan slice", "ratio": 1.0, "unit": None, "sentence": None, "comment": None, "additions": { "prep_time": 0, "ingredients": [], "steps": []} },
-    "chicken wing": { "substitution": "seitan slice", "ratio": 1.0, "unit": None, "sentence": None, "comment": None, "additions": { "prep_time": 0, "ingredients": [], "steps": []} },
-    "chicken leg": { "substitution": "seitan slice", "ratio": 1.0, "unit": None, "sentence": None, "comment": None, "additions": { "prep_time": 0, "ingredients": [], "steps": []} },
-    "chicken": { "substitution": "seitan", "ratio": 1.0, "unit": None, "sentence": None, "comment": None, "additions": { "prep_time": 0, "ingredients": [], "steps": []} },
-    "pork bones": { "substitution": "vegetable scraps", "ratio": 1.0, "unit": None, "sentence": None, "comment": None, "additions": { "prep_time": 0, "ingredients": [], "steps": []} },
-    "pork": { "substitution": "jackfruit", "ratio": 1.0, "unit": None, "sentence": None, "comment": None, "additions": { "prep_time": 0, "ingredients": [], "steps": []} },
-    "lamb": { "substitution": "eggplant", "ratio": 1.0, "unit": None, "sentence": None, "comment": None, "additions": { "prep_time": 0, "ingredients": [], "steps": []} },
-    "goat": { "substitution": "tempeh", "ratio": 1.0, "unit": None, "sentence": None, "comment": None, "additions": { "prep_time": 0, "ingredients": [], "steps": []} },
-    "turkey": { "substitution": "tofurky", "ratio": 1.0, "unit": None, "sentence": None, "comment": None, "additions": { "prep_time": 0, "ingredients": [], "steps": []} },
-    "veal": { "substitution": "beets", "ratio": 1.0, "unit": None, "sentence": None, "comment": None, "additions": { "prep_time": 0, "ingredients": [], "steps": []} },
-    "bone broth": { "substitution": "vegetable broth", "ratio": 1.0, "unit": None, "sentence": None, "comment": None, "additions": { "prep_time": 0, "ingredients": [], "steps": []} },
-    "bones": { "substitution": "vegetable scraps", "ratio": 1.0, "unit": None, "sentence": None, "comment": None, "additions": { "prep_time": 0, "ingredients": [], "steps": []} },
-}
-
-seafood_substitutions = {
-    "lobster": { "substitution": "vegan lobster", "ratio": 1.0, "unit": None, "sentence": None, "comment": None, "additions": { "prep_time": 0, "ingredients": [], "steps": []} },
-    "crab": { "substitution": "vegan crab", "ratio": 1.0, "unit": None, "sentence": None, "comment": None, "additions": { "prep_time": 0, "ingredients": [], "steps": []} },
-    "shrimp": { "substitution": "vegan shrimp", "ratio": 1.0, "unit": None, "sentence": None, "comment": None, "additions": { "prep_time": 0, "ingredients": [], "steps": []} },
-    "fish": { "substitution": "vegan fish", "ratio": 1.0, "unit": None, "sentence": None, "comment": None, "additions": { "prep_time": 0, "ingredients": [], "steps": []} },
-}
-
-vegetable_substitutions = {
-
-}
-
-healthy_substitutions = {
-
-}
-
-unhealthy_substitutions = {
-    
-}
-
-
-def construct_substitute_ingredient(previous, meat_product, substitute_dict, recipe_time, recipe_ingredients, recipe_steps, original_recipe_steps):
+def construct_substitute_ingredient(previous, substitutee_product, substitute_dict, recipe_time, recipe_ingredients, recipe_steps, original_recipe_steps):
     """Converts an ingredient with its substitute and adjusts the recipe using its parameters."""
     # Shallow copies to prevent changing after assignment.
     shallow_recipe_time = recipe_time
@@ -83,7 +22,7 @@ def construct_substitute_ingredient(previous, meat_product, substitute_dict, rec
     substitute_added_steps = substitute_dict["additions"]["steps"]
 
     # Update current ingredient name.
-    previous["name"] = previous["name"].lower().replace(meat_product, substitute_type)
+    previous["name"] = previous["name"].lower().replace(substitutee_product, substitute_type)
 
     # Adjust original ingredient quantity using substitute ratio and unit.
     previous["quantity"] = "{:.2f}".format((float(previous["quantity"]) * substitute_ratio))
@@ -96,7 +35,7 @@ def construct_substitute_ingredient(previous, meat_product, substitute_dict, rec
     if substitute_comment is not None:
         previous["comment"] = substitute_comment
     else:
-        previous["sentence"] = previous["sentence"].lower().replace(meat_product, substitute_type).capitalize()
+        previous["sentence"] = previous["sentence"].lower().replace(substitutee_product, substitute_type).capitalize()
 
     # Add additional prep time to recipe.
     shallow_recipe_time += substitute_added_time
@@ -111,34 +50,49 @@ def construct_substitute_ingredient(previous, meat_product, substitute_dict, rec
 
     return shallow_recipe_time, shallow_recipe_ingredients, shallow_recipe_steps
 
-pp = pprint.PrettyPrinter(indent=4)
-# pp.pprint(parsed_ingredients)
+def transform_recipe(substitutes_lexicon):
+    scraper = scrape_me("https://www.allrecipes.com/recipe/16167/beef-bourguignon-i/")
 
-for ingredient_dict in original_recipe_parsed_ingredients:
-    for meat_product, substitute_dict in meat_substitutions.items():
-        if meat_product in ingredient_dict["name"].lower():
-            updated_recipe_prep_time, updated_recipe_parsed_ingredients, updated_recipe_instructions = construct_substitute_ingredient(ingredient_dict, meat_product, substitute_dict, updated_recipe_prep_time, updated_recipe_parsed_ingredients, updated_recipe_instructions, original_recipe_instructions)
-            print("need to substitute:", ingredient_dict["name"])
-            break
+    # Original value recipe values.
+    original_recipe_prep_time = scraper.total_time()
+    # unprocessed_ingredients = scraper.ingredients()
+    # original_recipe_ingredients = [x.lower() for x in unprocessed_ingredients]
+    original_recipe_ingredients = scraper.ingredients()
+    original_recipe_parsed_ingredients = parse_multiple_ingredients(original_recipe_ingredients)
+    original_recipe_instructions = scraper.instructions_list()
 
+    # Updated value recipe values.
+    updated_recipe_prep_time = original_recipe_prep_time
+    updated_recipe_ingredients = original_recipe_ingredients.copy()
+    updated_recipe_parsed_ingredients = sorted(original_recipe_parsed_ingredients.copy(), key=lambda d: d['name'])
+    updated_recipe_instructions = original_recipe_instructions.copy()
 
-    # # Update all steps in recipe to replace references of meat products with substitute.
-    # # Must use original steps here to prevent from indexing overwritten steps.
-    # # Ex. "chicken" will overwrite "chicken broth" to "seitan broth".
-    # for index, step in enumerate(original_recipe_steps):
-    #     step = step.lower()
-    #     if meat_product in step:
-    #         temp_step = step.replace(meat_product, substitute_type).capitalize()
-    #         shallow_recipe_steps[index] = '. '.join(sentence.capitalize() for sentence in temp_step.split('. '))
+    for ingredient_dict in original_recipe_parsed_ingredients:
+        for substitutee, substitute_dict in substitutes_lexicon.items():
+            if substitutee in ingredient_dict["name"].lower():
+                updated_recipe_prep_time, updated_recipe_parsed_ingredients, updated_recipe_instructions = construct_substitute_ingredient(ingredient_dict, substitutee, substitute_dict, updated_recipe_prep_time, updated_recipe_parsed_ingredients, updated_recipe_instructions, original_recipe_instructions)
+                break
 
-# Final check through for any remaining references.
-for meat_product, substitute_dict in meat_substitutions.items():
-    for index, step in enumerate(updated_recipe_instructions):
-        step = step.lower()
-        if meat_product in step:
-            temp_step = step.replace(meat_product, substitute_dict["substitution"]).capitalize()
-            updated_recipe_instructions[index] = '. '.join(sentence.capitalize() for sentence in temp_step.split('. '))
+    # Final check through for any remaining references.
+    for substitutee, substitute_dict in substitutes_lexicon.items():
+        for index, step in enumerate(updated_recipe_instructions):
+            step = step.lower()
+            if substitutee in step:
+                temp_step = step.replace(substitutee, substitute_dict["substitution"]).capitalize()
+                updated_recipe_instructions[index] = '. '.join(sentence.capitalize() for sentence in temp_step.split('. '))
 
-pp.pprint(updated_recipe_parsed_ingredients)
-print(updated_recipe_instructions)
-print(updated_recipe_prep_time)
+    pp = pprint.PrettyPrinter(indent=4)
+    pp.pprint(updated_recipe_parsed_ingredients)
+    print(updated_recipe_instructions)
+    print(updated_recipe_prep_time)
+
+# def transform_to_vegetarian(url):
+# def transform_from_vegetarian(url):
+# def transform_to_healthy(url):
+# def transform_to_unhealty(url):
+# def transform_to_south_asian(url):
+# def transform_to_double(url):
+# def transform_to_half(url):
+
+transform_recipe(unhealthy_substitutions)
+
